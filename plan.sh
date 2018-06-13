bab613670ca6e8cd187599be83b23a64c3c46495
qubitrenegade <qubitrenegade@gmail.com>
2018-06-12 22:12:03 -0600

    cleanup plan.  add configurable ports.

--- plan.sh
+++ plan.sh
@@ -1,49 +1,21 @@
-# This file is the heart of your application's habitat.
-# See full docs at https://www.habitat.sh/docs/reference/plan-syntax/
 pkg_name=pd
 pkg_distname=pd-server
 pkg_origin=qubitrenegade
 pkg_version=2.0.3
-pkg_maintainer=""
+pkg_maintainer="QubitRenegade <qubitrenegade@gmail.com>"
 pkg_license=("MPL-2")
 pkg_source=http://download.pingcap.org/tidb-latest-linux-amd64.tar.gz
-pkg_shasum="37ddca23b740510c48510f637b64d3636dbff9f0c7614e6773b1e0c0d3841b1b"
+pkg_shasum=37ddca23b740510c48510f637b64d3636dbff9f0c7614e6773b1e0c0d3841b1b
 pkg_deps=(core/bash)
 pkg_build_deps=()
 pkg_bin_dirs=(bin)
 
-# Optional.
-# An associative array representing configuration data which should be gossiped to peers. The keys
-# in this array represent the name the value will be assigned and the values represent the toml path
-# to read the value.
-# pkg_exports=(
-#   [host]=srv.address
-#   [port]=srv.port
-#   [ssl-port]=srv.ssl.port
-# )
+pkg_exports=(
+  [client-port]=cfg.client-port
+  [peer-port]=cfg.peer-port
+)
 
-# Optional.
-# An array of `pkg_exports` keys containing default values for which ports that this package
-# exposes. These values are used as sensible defaults for other tools. For example, when exporting
-# a package to a container format.
-# pkg_exposes=(port ssl-port)
-
-# Optional.
-# An associative array representing services which you depend on and the configuration keys that
-# you expect the service to export (by their `pkg_exports`). These binds *must* be set for the
-# Supervisor to load the service. The loaded service will wait to run until it's bind becomes
-# available. If the bind does not contain the expected keys, the service will not start
-# successfully.
-# pkg_binds=(
-#   [database]="port host"
-# )
-
-
-# Optional.
-# Same as `pkg_binds` but these represent optional services to connect to.
-# pkg_binds_optional=(
-#   [storage]="port host"
-# )
+pkg_exposes=(client-port peer-port)
 
 # Optional.
 # pkg_svc_user="hab"
